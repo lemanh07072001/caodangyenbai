83347a48d7db08a6419f48046b2737a50f2dc2d8
manhle <lemanh.laravel@gmail.com>
2024-01-31 20:14:50 +0700

    feat: add route banner

---
+++ app/Models/Admin/Banner/Banner.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Admin\Banner;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Banner extends Model
+{
+    use HasFactory;
+}
